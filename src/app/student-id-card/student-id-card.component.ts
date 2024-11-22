9fc0944a7ef9f587614f0b7ab0dc42400b1e0188
venkateshthooti <venkatesh.bi2108@gmail.com>
2024-11-22 16:19:16 +0530

    Delete and pagination functionalities are added for studentIdcard component

--- src/app/student-id-card/student-id-card.component.ts
+++ src/app/student-id-card/student-id-card.component.ts
@@ -4,47 +4,75 @@ import { StudentIdCardService } from '../student-id-card.service';
 @Component({
   selector: 'app-student-id-card',
   templateUrl: './student-id-card.component.html',
-  styleUrls: ['./student-id-card.component.css']
+  styleUrls: ['./student-id-card.component.css'],
 })
 export class StudentIdCardComponent {
+  students: any = [];
 
-  students:any=[];
+  searchWord: string = '';
 
-  searchWord:string="";
-  
-  coulumnName:string="";
-  order:string="";
+  coulumnName: string = '';
+  order: string = '';
 
-  constructor(private _studentIdCardService:StudentIdCardService){
+  limit:number=0;
+  pageNumber:number=0;
+
+
+  constructor(private _studentIdCardService: StudentIdCardService) {
     _studentIdCardService.getAllstudents().subscribe(
-      (data:any)=>{
-          this.students=data
+      (data: any) => {
+        this.students = data;
       },
-      (err:any)=>{
-        alert("Internet server error")
+      (err: any) => {
+        alert('Internet server error');
       }
-    )
-
+    );
   }
-  filterComponent(){
-    this._studentIdCardService.getFilteredStudentIdCards(this.searchWord).subscribe(
+  filterComponent() {
+    this._studentIdCardService
+      .getFilteredStudentIdCards(this.searchWord)
+      .subscribe(
+        (data: any) => {
+          this.students = data;
+        },
+        (err: any) => {
+          alert('Internet server issue');
+        }
+      );
+  }
+  sortedComponent() {
+    this._studentIdCardService
+      .getSortedStudentIdcards(this.coulumnName, this.order)
+      .subscribe(
+        (data: any) => {
+          this.students = data;
+        },
+        (err: any) => {
+          alert('Internet server error');
+        }
+      );
+  }
+
+  deleteComponent(id: number) {
+    if (confirm('Are you sure you want to delete')) {
+      this._studentIdCardService.deleteService(id).subscribe(
+        (data: any) => {
+          alert('Deleted Successfully...!');
+        },
+        (error: any) => {
+          alert('Deletion failed...??');
+        }
+      );
+    }
+  }
+
+  paginationComponent(){
+    this._studentIdCardService.paginationService(this.limit,this.pageNumber).subscribe(
       (data:any)=>{
         this.students=data;
-      },
-      (err:any)=>{
-        alert("Internet server issue")
-      }
-    ) 
-  }
-  sortedComponent(){
-    this._studentIdCardService.getSortedStudentIdcards(this.coulumnName,this.order).subscribe(
-      (data:any)=>{
-        this.students=data;
-      },
-      (err:any)=>{
-        alert("Internet server error")
+      },(err:any)=>{
+        alert("pagination failed..!")
       }
     )
-
   }
 }
