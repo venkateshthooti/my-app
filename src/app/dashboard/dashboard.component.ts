cf309db441d0b6f21f76608167f27c413a0eca12
venkateshthooti <venkatesh.bi2108@gmail.com>
2024-11-28 09:56:26 +0530

    login redirect code modified if any user tries to enter using dashboard url

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,15 +10,16 @@ export class DashboardComponent {
   constructor(private _router: Router) {}
 
   logOutComponent() {
-    //go to login
+
+
+      if (confirm("Are you sure you want to logout")){
+            //go to login
     this._router.navigateByUrl('/login');
     // delete token
     sessionStorage.removeItem('token')
 
-    //   if (confirm("Are you sure you want to logout")){
-
-    //   }
-    //   alert("are you sure")
-    // }
+      }
+      alert("are you sure")
+    }
   }
-}
+
